d853e0213543c5c1c5a6c9794c025df8f692a286
rakafa <vytas.naus@gmail.com>
2020-02-23 16:58:16 +0200

    functions done

---
+++ main.py
@@ -0,0 +1,132 @@
+import yaml
+import os
+import inspect
+import collections
+import re
+
+
+def read_yaml():
+    # todo: check if dev number is put, also atleast 1 p2p
+    with open("config.yml", 'r') as file1:
+        yaml_file = yaml.safe_load(file1.read())
+        for i in yaml_file:
+            print(i)
+        return yaml_file
+
+
+def generate_config():
+    blank_cfg = inspect.cleandoc('''!
+        interface Gi1
+        no shut
+        !
+        line con 0
+         exec-timeout 0 0
+         logging synchronous
+         transport preferred none
+        !
+        ip tcp synwait-time 5
+        no ip icmp rate-limit unreachable
+        !
+        no ip domain-lookup
+        !''')
+    appended_cfg = blank_cfg
+
+    yaml_file = read_yaml()
+    for device in yaml_file:
+        appended_cfg += f'\nhostname R{device["name"]}\n!\n'
+
+        if device.get('loopback'):
+            loopback_ip = f'{device["name"]}.{device["name"]}.{device["name"]}.{device["name"]}'
+            appended_cfg += f'interface loopback 0\n' \
+                            f' ip address {loopback_ip} 255.255.255.255\n' \
+                            f' ip ospf network point-to-point\n!\n'
+
+        if device.get('external-loopback'):
+            loopback_ip = f'20.{device["name"]}.{device["name"]}.{device["name"]}'
+            appended_cfg += f'interface loopback 20\n' \
+                            f' ip address {loopback_ip} 255.255.255.255\n' \
+                            f' ip ospf network point-to-point\n!\n'
+
+        if device.get('p2p'):
+            for i in device['p2p']:
+                dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+                ip_address = f'10.0.{dot1q}.{device["name"]}'
+                appended_cfg += f'interface gi1.{dot1q}\n' \
+                                f' encapsulation dot1q {dot1q}\n' \
+                                f' ip address {ip_address} 255.255.255.0\n' \
+                                f' ip ospf network point-to-point\n!\n'
+
+        if device.get('external-p2p'):
+            for i in device['external-p2p']:
+                dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+                dot1q = 200 + int(dot1q)
+                ip_address = f'20.0.{dot1q}.{device["name"]}'
+                appended_cfg += f'interface gi1.{dot1q}\n' \
+                                f' encapsulation dot1q {dot1q}\n' \
+                                f' ip address {ip_address} 255.255.255.0\n' \
+                                f' ip ospf network point-to-point\n!\n'
+
+        if device.get('lan'):
+                dot1q = f'{device["lan"]}'
+                ip_address = f'10.0.{dot1q}.{device["name"]}'
+                appended_cfg += f'interface gi1.{dot1q}\n' \
+                                f' encapsulation dot1q {dot1q}\n' \
+                                f' ip address {ip_address} 255.255.255.0\n!\n'
+
+        if device.get('ospf'):
+            appended_cfg += 'router ospf 1\n'
+            for i in device['ospf']:
+                if bool(re.search('loopback', str(i))):
+                    network = f'{device["name"]}.{device["name"]}.{device["name"]}.{device["name"]}'
+                elif bool(re.search('lan', str(i))):
+                    network = f'10.0.{re.search("[0-9]+", i).group(0)}.{device["name"]}'
+                else:
+                    dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+                    network = f'10.0.{dot1q}.{i}'
+                appended_cfg += f' network {network} 0.0.0.0 area 0\n'
+            appended_cfg += '!\n'
+
+        if device.get('eigrp'):
+            appended_cfg += 'router eigrp 1\n no auto-summary\n'
+            for i in device['eigrp']:
+                if bool(re.search('loopback', str(i))):
+                    network = f'{device["name"]}.{device["name"]}.{device["name"]}.{device["name"]}'
+                else:
+                    dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+                    network = f'10.0.{dot1q}.{i}'
+                appended_cfg += f' network {network} 0.0.0.0\n'
+            appended_cfg += '!\n'
+
+        if device.get('ibgp'):
+            appended_cfg += 'router bgp 1\n'
+            for i in device['ibgp']:
+                dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+                neighbor = f'10.0.{dot1q}.{i}'
+                appended_cfg += f' neighbor {neighbor} remote-as 1\n'
+            appended_cfg += '!\n'
+
+        if device.get('ebgp'):
+            dot1q = f'{device["name"]}{i}' if device["name"] < i else f'{i}{device["name"]}'  # make asc
+            appended_cfg += f'router bgp {device["name"]}\n'
+            for i in device['ebgp']:
+                neighbor = f'20.0.{dot1q}.{i}'
+                appended_cfg += f' neighbor {neighbor} remote-as {i}\n'
+            appended_cfg += '!\n'
+
+        write_config(device['name'], appended_cfg)
+
+
+def write_config(filename: str, dev_config: str):
+    """write running configuration to file"""
+    if not os.path.exists('running'):
+        os.mkdir('running')
+    with open(f'running/{filename}', 'w') as output:
+        output.write(dev_config)
+
+
+def main():
+    generate_config()
+
+
+if __name__ == '__main__':
+    main()
